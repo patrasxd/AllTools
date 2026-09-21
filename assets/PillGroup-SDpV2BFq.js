36d4c8ffc26a72f3f80305ffd7eba5505297a2bc
patrasxd <patrasxd@users.noreply.github.com>
2026-09-21 15:05:41 +0000

    deploy: 280834588f669a0680a8b83eed65547976bcce69

---
+++ assets/PillGroup-SDpV2BFq.js
@@ -0,0 +1,1 @@
+import"./index-lFUQxaJ7.js";import{P as o}from"./PillGroup-DgZHFse0.js";const i=o;export{i as P};
--- assets/PillGroup-ixiLj7qc.js
+++
@@ -1,1 +0,0 @@
-import"./index-DCItZIMZ.js";import{P as o}from"./PillGroup-BCKwJesy.js";const i=o;export{i as P};
